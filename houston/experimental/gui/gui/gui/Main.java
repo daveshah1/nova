8c9b7306fee182fe2d7da7fa7787ee78f7d14edc
jamtholee <jameson.lee@gmx.com>
2013-09-25 19:59:27 +0100

    Revert "Meh"
    This reverts commit 564270c3b118b66e3fe7e008a8d5d9082352564a.

--- houston/experimental/gui/gui/gui/Main.java
+++ houston/experimental/gui/gui/gui/Main.java
@@ -3,20 +3,10 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JMenuItem;
-import javax.swing.JTabbedPane;
-import java.awt.BorderLayout;
 
 public class Main {
 
-	private JFrame frmNovaControlPanel;
+	private JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -26,7 +16,7 @@ public class Main {
 			public void run() {
 				try {
 					Main window = new Main();
-					window.frmNovaControlPanel.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,31 +35,9 @@ public class Main {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmNovaControlPanel = new JFrame();
-		frmNovaControlPanel.setTitle("Nova Control Panel");
-		frmNovaControlPanel.setBounds(100, 100, 1280, 800);
-		frmNovaControlPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout( new BorderLayout() );
-		frmNovaControlPanel.getContentPane().add( topPanel );		
-		
-		createPage1();
-		createPage2();
-		
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		frmNovaControlPanel.getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
-		tabbedPane = new JTabbedPane();
-		tabbedPane.addTab( "Main", panel1 );
-		tabbedPane.addTab( "Debug", panel2 );
-		topPanel.add( tabbedPane, BorderLayout.CENTER );		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1024, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	public void createPage1() {
-	}
-	
-	public void createPage2(){
-	}
+
 }
