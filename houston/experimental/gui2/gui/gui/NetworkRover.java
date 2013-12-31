6c5f11a27b6ec009e4af2ba4c4a83bf4cbf8473b
David Shah <davey1576@gmail.com>
2013-12-31 16:41:13 +0000

    Added initial NetworkRover class

--- houston/experimental/gui2/gui/gui/NetworkRover.java
+++ houston/experimental/gui2/gui/gui/NetworkRover.java
@@ -1,9 +1,57 @@
 package gui;
 
+import javax.swing.JOptionPane;
+
 public class NetworkRover extends Rover {
-
+	
+	private NetworkSender network;
+	
 	public NetworkRover() {
-		// TODO Auto-generated constructor stub
-	}
 
+	}
+	
+	boolean begin(String hostname) {
+		network = new NetworkSender(hostname);
+		if(testNetwork() == NetworkStatus.OK) {
+			//Initialise video stream
+			return true;
+		} else {
+			disconnect();
+			return false;
+		}
+	}
+	
+	NetworkStatus testNetwork() {
+		return network.sendCommand(new NetworkCommand("PI")).getStatus();
+	}
+	
+	void disconnect() {
+		//Stop video stream
+		//Any other closing down stuff
+	}
+	
+	void moveToPosition(Position p) {
+		targetPosition = p;
+		atTargetPosition = false;
+		NetworkCommand cmd = new NetworkCommand("GT",p.getLat() + " "  + p.getLon());
+		NetworkStatus status = network.sendCommand(cmd).getStatus();
+		switch(status) {
+		case OK:
+			break;
+		case COMMUNICATION_ERROR:
+			//Try once more
+			NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
+			if(newStatus == NetworkStatus.OK) break;
+			//Otherwise drop into error handling
+		default:
+			targetPosition = currentPosition;
+			atTargetPosition = true;
+			JOptionPane.showMessageDialog(null,"Network error: " + status.toString());
+			break;
+		}
+	}
+	
+	void stopRover() {
+		
+	}
 }
