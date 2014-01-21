8878b518a88e1021ab6ddc2f3f2895c75b537ab0
David Shah <davey1576@gmail.com>
2014-01-21 21:28:46 +0000

    Landing module radio handling code

--- houston/experimental/gui2/gui/gui/BaseStationCommunications.java
+++ houston/experimental/gui2/gui/gui/BaseStationCommunications.java
@@ -1,5 +1,11 @@
 package gui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
 import gnu.io.NoSuchPortException;
@@ -14,6 +20,8 @@ import gnu.io.UnsupportedCommOperationException;
  */
 public class BaseStationCommunications {
 	private SerialPort serialPort;
+	private BufferedReader serialPortReader;
+	private PrintWriter serialPortWriter;
 	public BaseStationCommunications() {
 		// TODO Auto-generated constructor stub
 	}
@@ -28,11 +36,15 @@ public class BaseStationCommunications {
 			CommPort commPort = portIdentifier.open("novaController",500);
 			serialPort = (SerialPort) commPort;
 			serialPort.setSerialPortParams(57600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+			serialPortReader = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+			serialPortWriter = new PrintWriter(new OutputStreamWriter(serialPort.getOutputStream()));
 		} catch (NoSuchPortException e) {
 			return false;
 		} catch (PortInUseException e) {
 			return false;
 		} catch (UnsupportedCommOperationException e) {
+			return false;
+		} catch (IOException e) {
 			return false;
 		}
 		
@@ -41,13 +53,14 @@ public class BaseStationCommunications {
 	
 	//Close the serial port
 	public void stopCommunications() {
-		
+		serialPort.close();
 	}
 	
 	//True = OK
 	//False = Error
 	//Sets WiFi antenna pan (left/right) servo angle
-	public boolean setAntennaPan(double theta) {
+	public boolean setAntennaPan(int theta) {
+		serialPortWriter.println("A P " + theta);
 		return true;
 	}
 	
@@ -55,14 +68,21 @@ public class BaseStationCommunications {
 	//False = Error
 	//Sets WiFi antenna tilt (up/down) servo angle
 	public boolean setAntennaTilt(double theta) {
+		serialPortWriter.println("A T " + theta);
 		return true;
 	}
 	
 	public String getAvailableRadioData() {
-		return "";
+		serialPortWriter.println("R R");
+		try {
+			return serialPortReader.readLine();
+		} catch (IOException e) {
+			return "";
+		}
 	}
 	
 	public boolean sendRadio(String data) {
+		serialPortWriter.println("R W " + data);
 		return false;
 	}
 }
--- houston/experimental/gui2/gui/gui/LandingModule.java
+++ houston/experimental/gui2/gui/gui/LandingModule.java
@@ -1,6 +1,21 @@
 package gui;
 
 import java.util.Vector;
+
+/*
+ * Radio sentence format
+ * BdataE
+ * data: temp,pressure,gpsAvailable,lat,long,alt,deployed (comma separated)
+ * e.g. sentence: B175,100000,1,51.487556,-0.2381855,100,1E
+ * temp: temperature in degrees C * 10
+ * pressure: pressure in Pa
+ * gpsAvailable: 1 if GPS fix available, 0 otherwise
+ * lat: GPS latitude
+ * long: GPS longitude
+ * alt: GPS (not barometric) altitude
+ * deployed: 1 if rover deployed, 0 if not
+ */
+
 public class LandingModule {
 	public enum DeploymentStatus {
 		LOADED, DEPLOYED, UNKNOWN
@@ -10,8 +25,9 @@ public class LandingModule {
 	public Position currentPosition;
 	protected TPData currentData;
 	public DeploymentStatus roverDeployed;
-	public boolean connected;
-	
+	public boolean connected = false, running = false, gpsAvailable = false;
+	String radioBuffer = "";
+	private BaseStationCommunications baseStation;
 	public void attachListener(LandingModuleListener l) {
 		listeners.add(l);
 	};
@@ -24,14 +40,49 @@ public class LandingModule {
 
 	}
 
-	public void startCommunications(BaseStationCommunications baseStation) {
-
+	public void startCommunications(BaseStationCommunications baseArduinoComms) {
+		baseStation = baseArduinoComms;
+		running = false;
 	}
 
 	public void stopCommunications() {
-
+		running = false;
 	}
+	
+	public void update() {
+		if(running) {
+			 boolean packetRecieved = false;
+			 String newData = baseStation.getAvailableRadioData();
+			 radioBuffer += newData;
+			 while(true) {
+				 int endOfSentence = radioBuffer.indexOf("E");
+				 if(endOfSentence == -1) break;
+				 String sentence = radioBuffer.substring(0,endOfSentence); //Split at ending delimiter
+				 radioBuffer = radioBuffer.substring(endOfSentence+1); //Put remainder back into buffer
+				 if(!sentence.startsWith("B")) continue; //Not a full sentence, discard
+				 //Trim start letter
+				 sentence = sentence.substring(1);
+				 String splitSentence[] = sentence.split(",");
+				 if(splitSentence.length < 7) continue;
+				 currentData.temperature = Double.parseDouble(splitSentence[0]) / 10;
+				 currentData.pressure = Double.parseDouble(splitSentence[1]);
+				 gpsAvailable = Boolean.parseBoolean(splitSentence[2]);
+				 if(gpsAvailable) {
+					 currentPosition = new Position( Double.parseDouble(splitSentence[3]), 
+							 Double.parseDouble(splitSentence[4]));
+					 
+				 }
+				 packetRecieved = true;
 
+			 }
+			 if(packetRecieved) {
+				 firePositionUpdate();
+				 fireDataUpdate();
+				 fireStatusUpdate();
+			 }
+		}
+	}
+	
 	protected void firePositionUpdate() {
 		for (LandingModuleListener l : listeners) {
 			l.positionUpdated(currentPosition, this);
