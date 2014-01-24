efae83c8e2a937981cedb51cbd250b5fc2d49279
David Shah <davey1576@gmail.com>
2014-01-24 16:58:15 +0000

    Improved NetworkRover error handling

--- houston/experimental/gui2/gui/gui/NetworkRover.java
+++ houston/experimental/gui2/gui/gui/NetworkRover.java
@@ -44,51 +44,57 @@ public class NetworkRover extends Rover {
 	}
 	
 	public void moveToPosition(Position p) {
-		targetPosition = p;
-		atTargetPosition = false;
-		NetworkCommand cmd = new NetworkCommand("GT",p.getLat() + " "  + p.getLon());
-		NetworkStatus status = network.sendCommand(cmd).getStatus();
-		switch(status) {
-		case OK:
-			break;
-		case COMMUNICATION_ERROR:
-			//Try once more
-			NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
-			if(newStatus == NetworkStatus.OK) break;
-			//Otherwise drop into error handling
-		default:
-			targetPosition = currentPosition;
-			atTargetPosition = true;
-			JOptionPane.showMessageDialog(null,"Network error: " + status.toString());
-			break;
-		}
+		if(online) {
+			targetPosition = p;
+			atTargetPosition = false;
+			NetworkCommand cmd = new NetworkCommand("GT",p.getLat() + " "  + p.getLon());
+			NetworkStatus status = network.sendCommand(cmd).getStatus();
+			switch(status) {
+			case OK:
+				break;
+			case COMMUNICATION_ERROR:
+				//Try once more
+				NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
+				if(newStatus == NetworkStatus.OK) break;
+				//Otherwise drop into error handling
+			default:
+				targetPosition = currentPosition;
+				atTargetPosition = true;
+				JOptionPane.showMessageDialog(null,"Network error: " + status.toString());
+				break;
+			}
+		};
 	}
 	
 	public Position getPosition() throws NetworkException {
-		NetworkCommand cmd = new NetworkCommand("CL");
-		NetworkResponse response = network.sendCommand(cmd);
-		switch(response.getStatus()) {
-		case OK:
-			break;
-		case COMMUNICATION_ERROR:
-			//Try once more
-			NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
-			if(newStatus == NetworkStatus.OK) break;
-			//Otherwise drop into error handling
-		default:
-			targetPosition = currentPosition;
-			atTargetPosition = true;
-			throw new NetworkException(response.getStatus());
-		}
-		String splitStr[] = response.getPayload().split("\\s+");
-		if(splitStr.length < 2) throw new NetworkException(NetworkStatus.RESPONSE_INVALID,
-				"Position string returned: " + response.getPayload());
-		try {
-			double lat = Double.parseDouble(splitStr[0]);
-			double lon = Double.parseDouble(splitStr[1]);
-			return new Position(lat,lon);
-		} catch(Exception e) {
-			throw new NetworkException(NetworkStatus.RESPONSE_INVALID,"Position string returned: " + response.getPayload());
+		if(online) {
+			NetworkCommand cmd = new NetworkCommand("CL");
+			NetworkResponse response = network.sendCommand(cmd);
+			switch(response.getStatus()) {
+			case OK:
+				break;
+			case COMMUNICATION_ERROR:
+				//Try once more
+				NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
+				if(newStatus == NetworkStatus.OK) break;
+				//Otherwise drop into error handling
+			default:
+				targetPosition = currentPosition;
+				atTargetPosition = true;
+				throw new NetworkException(response.getStatus());
+			}
+			String splitStr[] = response.getPayload().split("\\s+");
+			if(splitStr.length < 2) throw new NetworkException(NetworkStatus.RESPONSE_INVALID,
+					"Position string returned: " + response.getPayload());
+			try {
+				double lat = Double.parseDouble(splitStr[0]);
+				double lon = Double.parseDouble(splitStr[1]);
+				return new Position(lat,lon);
+			} catch(Exception e) {
+				throw new NetworkException(NetworkStatus.RESPONSE_INVALID,"Position string returned: " + response.getPayload());
+			}
+		} else {
+			throw new NetworkException(NetworkStatus.ROVER_OFFLINE);
 		}
 	}
 	
@@ -112,84 +118,92 @@ public class NetworkRover extends Rover {
 	}
 	
 	public void moveManually(MoveOperation how) {
-		String opcode = "";
-		switch(how) {
-		case FORWARDS:
-			opcode = "F";
-			break;
-		case BACKWARDS:
-			opcode = "B";
-			break;
-		case LEFT_TURN:
-			opcode = "L";
-			break;
-		case RIGHT_TURN:
-			opcode = "R";
-			break;
-		}
-
-		NetworkCommand cmd = new NetworkCommand("MV",opcode);
-		NetworkStatus status = network.sendCommand(cmd).getStatus();
-		switch(status) {
-		case OK:
-			break;
-		case COMMUNICATION_ERROR:
-			//Try once more
-			NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
-			if(newStatus == NetworkStatus.OK) break;
-			//Otherwise drop into error handling
-		default:
-			throwError("Couldn't move - error: " + status.toString());
-			return;
-		}
-		targetPosition = currentPosition;
-		atTargetPosition = true;
+		if(online) {
+			String opcode = "";
+			switch(how) {
+			case FORWARDS:
+				opcode = "F";
+				break;
+			case BACKWARDS:
+				opcode = "B";
+				break;
+			case LEFT_TURN:
+				opcode = "L";
+				break;
+			case RIGHT_TURN:
+				opcode = "R";
+				break;
+			}
+	
+			NetworkCommand cmd = new NetworkCommand("MV",opcode);
+			NetworkStatus status = network.sendCommand(cmd).getStatus();
+			switch(status) {
+			case OK:
+				break;
+			case COMMUNICATION_ERROR:
+				//Try once more
+				NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
+				if(newStatus == NetworkStatus.OK) break;
+				//Otherwise drop into error handling
+			default:
+				throwError("Couldn't move - error: " + status.toString());
+				return;
+			}
+			targetPosition = currentPosition;
+			atTargetPosition = true;
+		};
 	}
 	
 	public void stopRover() {
-		NetworkCommand cmd = new NetworkCommand("ST");
-		NetworkStatus status = network.sendCommand(cmd).getStatus();
-		switch(status) {
-		case OK:
-			break;
-		case COMMUNICATION_ERROR:
-			//Try once more
-			NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
-			if(newStatus == NetworkStatus.OK) break;
-			//Otherwise drop into error handling
-		default:
-			throwError("Couldn't stop - error: " + status.toString());
-			return;
-		}
-		targetPosition = currentPosition;
-		atTargetPosition = true;
+		if(online) {
+			NetworkCommand cmd = new NetworkCommand("ST");
+			NetworkStatus status = network.sendCommand(cmd).getStatus();
+			switch(status) {
+			case OK:
+				break;
+			case COMMUNICATION_ERROR:
+				//Try once more
+				NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
+				if(newStatus == NetworkStatus.OK) break;
+				//Otherwise drop into error handling
+			default:
+				throwError("Couldn't stop - error: " + status.toString());
+				return;
+			}
+			targetPosition = currentPosition;
+			atTargetPosition = true;
+		};
 	}
 	
 	public TPData getTP() throws NetworkException {
-		NetworkCommand cmd = new NetworkCommand("TP");
-		NetworkResponse response = network.sendCommand(cmd);
-		switch(response.getStatus()) {
-		case OK:
-			break;
-		case COMMUNICATION_ERROR:
-			//Try once more
-			NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
-			if(newStatus == NetworkStatus.OK) break;
-			//Otherwise drop into error handling
-		default:
-			targetPosition = currentPosition;
-			atTargetPosition = true;
-			throw new NetworkException(response.getStatus());
-		}
-		String splitStr[] = response.getPayload().split("\\s+");
-		if(splitStr.length < 2) throw new NetworkException(NetworkStatus.RESPONSE_INVALID,
-				"TP string returned: " + response.getPayload());
-		try {
-			double t = Double.parseDouble(splitStr[0]);
-			double p = Double.parseDouble(splitStr[1]);
-			return new TPData(t,p);
-		} catch(Exception e) {
-			throw new NetworkException(NetworkStatus.RESPONSE_INVALID,"TP string returned: " + response.getPayload());
+		if(online) {
+			NetworkCommand cmd = new NetworkCommand("TP");
+			NetworkResponse response = network.sendCommand(cmd);
+			switch(response.getStatus()) {
+			case OK:
+				break;
+			case COMMUNICATION_ERROR:
+				//Try once more
+				NetworkStatus newStatus = network.sendCommand(cmd).getStatus();
+				if(newStatus == NetworkStatus.OK) break;
+				//Otherwise drop into error handling
+			default:
+				targetPosition = currentPosition;
+				atTargetPosition = true;
+				throw new NetworkException(response.getStatus());
+			}
+			String splitStr[] = response.getPayload().split("\\s+");
+			if(splitStr.length < 2) throw new NetworkException(NetworkStatus.RESPONSE_INVALID,
+					"TP string returned: " + response.getPayload());
+			try {
+				double t = Double.parseDouble(splitStr[0]);
+				double p = Double.parseDouble(splitStr[1]);
+				return new TPData(t,p);
+			} catch(Exception e) {
+				throw new NetworkException(NetworkStatus.RESPONSE_INVALID,"TP string returned: " + response.getPayload());
+			}
+		} else {
+			throw new NetworkException(NetworkStatus.ROVER_OFFLINE);
 		}
 	}
 }
