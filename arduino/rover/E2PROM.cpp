35a202dd0d6ecabc87f950b23524791a290bffcf
David Shah <davey1576@gmail.com>
2014-02-05 20:41:55 +0000

    Work on EEPROM code

---
+++ arduino/rover/E2PROM.cpp
@@ -0,0 +1,113 @@
+#include "E2PROM.h"
+E2PROM::E2PROM() {
+	current_read_page = 0;
+};
+
+void E2PROM::begin_logging() {
+	unsigned char firstPage[7];
+	bool status == read_page(0, firstPage);
+	//Check if devices are initialised
+	if((!status) || (firstPage[0] != 0x4e)) {
+		//Try again to be sure
+		status == read_page(0, firstPage);
+		//Must be blank, format	
+		if((!status) || (firstPage[0] != 0x4e)) {
+			Serial.println("#EEPROM blank, formatting");			
+			unsigned char newPage[7] = {0x4e, 0x4f, 0x56,0x00, 0x01, 0x00, 0x00};
+			write_page(0, newPage);
+		};
+	};
+	current_write_page = (newPage[3] << 8) + newPage[4];
+	unsigned char page_break[7] = {0x50, 0x41, 0x47, 0x45, 0x00, 0x00, 0x00};
+};
+
+unsigned char E2PROM::read_byte(int device, unsigned long address) {
+	Wire.beginTransmission(DEVICE_ADDRESS(address >> 16,device));
+	Wire.write((address >> 9) & 0xFF); //High byte
+	Wire.write(address & 0xFF); //Low byte
+	Wire.endTransmission();
+	Wire.requestFrom(DEVICE_ADDRESS(address >> 16,device),1);
+	if(Wire.available()) {
+		return Wire.read();
+	} else {
+		return 0x00;
+	};
+};
+
+void E2PROM::write_byte(int device, unsigned long address, unsigned char data) {
+	Wire.beginTransmission(DEVICE_ADDRESS(address >> 16,device));
+	Wire.write((address >> 9) & 0xFF); //High address byte
+	Wire.write(address & 0xFF); //Low address byte
+	Wire.write(data);
+	Wire.endTransmission();
+	delay(1);
+};
+
+
+bool E2PROM::read_page(unsigned int page, unsigned char *buffer) {
+	int byteCount = 0;	
+	unsigned char page[8];
+	Wire.beginTransmission(DEVICE_ADDRESS((page >> 13) & 0x01,page >> 14));
+	Wire.write((page >> 5) & 0xFF); //High address byte
+	Wire.write((address * 8) & 0xFF); //Low address byte
+	Wire.endTransmission();
+	Wire.requestFrom(DEVICE_ADDRESS((page >> 13) & 0x01,page >> 14),8);
+	while((Wire.available()) && (byteCount < 8)) {
+		page[byteCount] = Wire.read();
+		if(byteCount < 7) buffer[byteCount] = page[byteCount];
+		byteCount++;
+	};
+	if(byteCount != 8) {
+		return false;
+	};
+	unsigned long tmpsum;
+	for(int i = 0;i < 7;i++) {
+		tmpsum += (7 - i) * page[i];
+	};
+	unsigned char checksum;
+	checksum = tmpsum % 0xFF;
+	if(checksum != page[7]) {
+		return false;
+	} else {
+		return true;
+	};
+};	
+
+void E2PROM::write_page(unsigned int page, unsigned char *data) {
+	unsigned long tmpsum;
+	for(int i = 0;i < 7;i++) {
+		tmpsum += (7 - i) * data[i];
+		write_byte(page >> 14, (page & 0x4000) + i,data[i]);
+	};
+	unsigned char checksum;
+	checksum = tmpsum % 0xFF;
+	write_byte(page >> 14, (page & 0x4000) + 7,checksum);
+};
+
+void E2PROM::write_next_page(unsigned char *data) {
+	write_page(data,current_write_page);
+	current_write_page++;
+};
+
+void E2PROM::begin_read() {
+	current_read_page = 0;
+};
+
+bool E2PROM::read_next_page(unsigned char *buffer) {
+	bool status = read_page(current_read_page,buffer);
+	current_read_page++;
+	return status;
+};
+
+bool E2PROM::is_end() {
+	if(current_read_page == current_write_page) {
+		return true;
+	} else {
+		return false;
+	};
+};
+
+unsigned int E2PROM::get_page_to_write() {
+	return current_write_page();
+};
+
--- arduino/rover/E2PROM.h
+++ arduino/rover/E2PROM.h
@@ -1,3 +1,4 @@
+#include "Wire.h";
 /*EEPROM definitions
 Devices split up into 'blocks' of 8 bytes
 Device 0, block 0:
@@ -5,14 +6,12 @@ Device 0, block 0:
 2 bytes page pointer
 2 bytes unused (null for now)
 1 byte checksum (see below)
-Device 1, block 0:
-8 bytes start: 4e 4f 56 41 00 00 00 AE
 
 'Page breaks' separate logging sessions
 8 bytes: 50 41 47 45 00 00 00 2D
 
 Data pages: 7 bytes data, 1 byte checksum
-Data: 3 bytes pressure (Pa), 2 bytes temperature (C*10), 1 byte Vbat (V*20), 1 byte reserved for future use
+Data: 3 bytes pressure (Pa), 2 bytes temperature (C*10), 2 byte timestamp (millis since last log)
 Checksum: 7*[Byte 0] + 6*[Byte 1]  + 5*[Byte 2] ... + 1*[Byte 7] modulo 256
 
 Pressure & Vbat unsigned
@@ -23,14 +22,14 @@ Bits 0 is device ID
 remaining bits are page on device
 actual address = page id * 8
 */
+#define DEVICE_ADDRESS(b, n) (0x50 | b << 2 | n << 1) 
 class E2PROM {
 private:
 	unsigned int current_write_page;
 	unsigned int current_read_page;
-	unsigned int end_of_pages;
 	void write_byte(int device, unsigned long address, unsigned char data);
 	unsigned char read_byte(int device, unsigned long address);
-	
+
 public:
 	E2PROM(); //Constructor
 	bool check_status(); //Check devices are online
@@ -41,5 +40,5 @@ public:
 	void begin_read(); //Reset read pointer
 	bool read_next_page(unsigned char *buffer); //Read next page (sequential reads). True=ok, False=error
 	bool is_end(); //True if end of EEPROM
-	unsigned int current_write_page(); //Get current page to write to
+	unsigned int get_page_to_write(); //Get current page to write to
 };
