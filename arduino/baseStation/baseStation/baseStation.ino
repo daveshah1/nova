659fcb19c6c43d3f9068ed368c959792df2b489c
David Shah <davey1576@gmail.com>
2014-02-26 19:10:30 +0000

    Added some compass & LCD code

--- arduino/baseStation/baseStation/baseStation.ino
+++ arduino/baseStation/baseStation/baseStation.ino
@@ -1,28 +1,73 @@
+#include <LSM303.h>
+#include <Adafruit_ST7735.h>
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "lcd.h"
+
+/*--PIN DEFINITIONS--*/
+//LCD
+#define cs   53
+#define dc   46
+#define rst  44
+//RF
 #define SET 16
 #define AUX 17
-int readline(int readch, char *buffer, int len)
-{
-  static int pos = 0;
-  int rpos;
+/*-------------------*/
+
+LSM303 compass;
+Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+
+float overSampleHeading() {
+    float heading = 0;
+  for(int i = 0;i<10;i++) {
+   compass.read();
+   heading += compass.heading();
+   delay(1);
+  };
+ // Serial.println(heading / 10);
+  return (heading / 10.0);
+};
+
+//Display 'Team Nova' logo with text below
+void drawSplash(char *text) {
+  tft.fillScreen(ST7735_WHITE);
   
-  if (readch > 0) {
-    switch (readch) {
-      case '\n': // Ignore new-lines
-        break;
-      case '\r': // Return on CR
-        rpos = pos;
-        pos = 0;  // Reset position index ready for next time
-        return rpos;
-      default:
-        if (pos < len-1) {
-          buffer[pos++] = readch;
-          buffer[pos] = 0;
-        }
-    }
-  }
-  // No end of line has been found, so return -1.
-  return -1;
-}
+ /// testlines(tft.Color565(255, 0, 255));
+  int address = 0;
+  for(int i = 0; i < 41; i++) { //41 lines
+     for(int j = 0;j<120;j++) { //120 cols
+      tft.drawPixel(j+4, i+10,tft.Color565(pgm_read_byte_near(pixel_data+address),
+                                           pgm_read_byte_near(pixel_data+address+1),
+                                           pgm_read_byte_near(pixel_data+address+2)));
+    // tft.drawPixel(j+4,i+10,tft.Color565(255,0,0));
+      address += 3;
+      //delay(1);   
+     };
+  };
+  tft.setTextColor(ST7735_BLACK);
+  //FIXME - center text on screen
+  tft.setCursor(23,80);
+  tft.setTextSize(2);
+  tft.println(text);
+};
+
+//For drawing position guidance arrows
+void drawArrows(boolean up, boolean down, boolean left, boolean right, uint16_t color) {
+  if(up) {
+    tft.fillTriangle(48,65,80,65,64,33,color);
+  };
+  if(down) {
+    tft.fillTriangle(48,95,80,95,64,127,color);
+  };
+  if(left) {
+    tft.fillTriangle(48,65,48,95,16,80,color);
+  };
+   if(right) {
+    tft.fillTriangle(80,65,80,95,112,80,color);
+  };
+};
+
 void setup() {
   pinMode(19,INPUT);
   Serial1.begin(9600);
@@ -31,7 +76,17 @@ void setup() {
   pinMode(SET, OUTPUT);
   digitalWrite(AUX,LOW);
 
+
   digitalWrite(SET,HIGH);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  //If we have time, change these for maximum accuracy. Probably not critical though.
+  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};  
+  
+  tft.initR(INITR_BLACKTAB); 
+  tft.fillScreen(ST7735_WHITE);
   delay(100);
   digitalWrite(SET,LOW);
   delay(1);
@@ -48,10 +103,9 @@ void setup() {
   delay(200);
   digitalWrite(SET,HIGH);
   delay(200);
+  drawSplash("OFFLINE");
 };
 
 void loop() {
-  if(Serial1.available() > 0) {
-    Serial.write(Serial1.read());
-  };
+  //Code here...
 };
---
+++ arduino/baseStation/graphicstest/graphicstest.ino
@@ -0,0 +1,375 @@
+/***************************************************
+  This is an example sketch for the Adafruit 1.8" SPI display.
+  This library works with the Adafruit 1.8" TFT Breakout w/SD card
+  ----> http://www.adafruit.com/products/358
+  as well as Adafruit raw 1.8" TFT display
+  ----> http://www.adafruit.com/products/618
+ 
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+// For the breakout, you can use any (4 or) 5 pins
+//#define sclk 4
+//#define mosi 5
+//#define cs   6
+//#define dc   7
+//#define rst  8  // you can also connect this to the Arduino reset
+
+//Use these pins for the shield!
+
+#define cs   53
+#define dc   46
+#define rst  44 // you can also connect this to the Arduino reset
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+#include <Wire.h>
+#include <LSM303.h>
+
+LSM303 compass;
+
+
+#include "lcd.h"
+#if defined(__SAM3X8E__)
+    #undef __FlashStringHelper::F(string_literal)
+    #define F(string_literal) string_literal
+#endif
+
+// Option 1: use any pins but a little slower
+//Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rst);
+
+// Option 2: must use the hardware SPI pins
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+float p = 3.1415926;
+
+ 
+#define Neutral 0
+#define Press 1
+#define Up 2
+#define Down 3
+#define Right 4
+#define Left 5
+ 
+// Check the joystick position
+int CheckJoystick()
+{
+  int joystickState = analogRead(3);
+  
+  if (joystickState < 50) return Left;
+  if (joystickState < 150) return Down;
+  if (joystickState < 250) return Press;
+  if (joystickState < 500) return Right;
+  if (joystickState < 650) return Up;
+  return Neutral;
+}
+
+float overSampleHeading() {
+    float heading = 0;
+  for(int i = 0;i<10;i++) {
+   compass.read();
+   heading += compass.heading();
+   delay(1);
+  };
+ // Serial.println(heading / 10);
+  return (heading / 10.0);
+};
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("hello!");
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  
+  /*
+  Calibration values; the default values of +/-32767 for each axis
+  lead to an assumed magnetometer bias of 0. Use the Calibrate example
+  program to determine appropriate values for your particular unit.
+  */
+  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+  // Our supplier changed the 1.8" display slightly after Jan 10, 2012
+  // so that the alignment of the TFT had to be shifted by a few pixels
+  // this just means the init code is slightly different. Check the
+  // color of the tab to see which init code to try. If the display is
+  // cut off or has extra 'random' pixels on the top & left, try the
+  // other option!
+  // If you are seeing red and green color inversion, use Black Tab
+
+  // If your TFT's plastic wrap has a Black Tab, use the following:
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+  // If your TFT's plastic wrap has a Red Tab, use the following:
+  //tft.initR(INITR_REDTAB);   // initialize a ST7735R chip, red tab
+  // If your TFT's plastic wrap has a Green Tab, use the following:
+  //tft.initR(INITR_GREENTAB); // initialize a ST7735R chip, green tab
+
+  Serial.println("init");
+  
+  uint16_t time = millis();
+  tft.fillScreen(ST7735_WHITE);
+  
+ /// testlines(tft.Color565(255, 0, 255));
+  int address = 0;
+  for(int i = 0; i < 41; i++) {
+     for(int j = 0;j<120;j++) {
+      tft.drawPixel(j+4, i+10,tft.Color565(pgm_read_byte_near(pixel_data+address),
+                                           pgm_read_byte_near(pixel_data+address+1),
+                                           pgm_read_byte_near(pixel_data+address+2)));
+    // tft.drawPixel(j+4,i+10,tft.Color565(255,0,0));
+      address += 3;
+      //delay(1);   
+     };
+  };
+  tft.setTextColor(ST7735_BLACK);
+  tft.setCursor(23,80);
+  tft.setTextSize(2);
+  tft.println("OFFLINE");
+  
+  delay(2000);
+   tft.fillScreen(ST7735_WHITE);
+//ldrawArrows(true,true,true,true,ST7735_RED);
+
+
+}
+
+void drawArrows(boolean up, boolean down, boolean left, boolean right, uint16_t color) {
+  if(up) {
+    tft.fillTriangle(48,65,80,65,64,33,color);
+  };
+  if(down) {
+    tft.fillTriangle(48,95,80,95,64,127,color);
+  };
+  if(left) {
+    tft.fillTriangle(48,65,48,95,16,80,color);
+  };
+   if(right) {
+    tft.fillTriangle(80,65,80,95,112,80,color);
+  };
+};
+
+void loop() {
+     tft.fillScreen(ST7735_WHITE);
+/*
+    drawArrows(true,false,false,false,ST7735_RED);
+  delay(500);
+     tft.fillScreen(ST7735_WHITE);
+
+    drawArrows(false,true,false,false,ST7735_RED);
+  delay(500);
+     tft.fillScreen(ST7735_WHITE);
+
+    drawArrows(false,false,true,false,ST7735_RED);
+  delay(500);
+     tft.fillScreen(ST7735_WHITE);
+
+    drawArrows(false,false,false,true,ST7735_RED);
+  delay(500);
+     tft.fillScreen(ST7735_WHITE);
+
+  drawArrows(true,true,true,true,ST7735_RED);
+  
+  */
+  float osH = overSampleHeading();
+  
+    tft.setTextColor(ST7735_BLACK);
+  tft.setCursor(25,80);
+  tft.setTextSize(2);
+  tft.println(osH,1);
+  delay(500);
+  
+  
+}
+
+void testlines(uint16_t color) {
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, 0, x, tft.height()-1, color);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, 0, tft.width()-1, y, color);
+  }
+
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, 0, 0, y, color);
+  }
+
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, tft.height()-1, x, 0, color);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+  }
+
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+  }
+}
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void testfastlines(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t y=0; y < tft.height(); y+=5) {
+    tft.drawFastHLine(0, y, tft.width(), color1);
+  }
+  for (int16_t x=0; x < tft.width(); x+=5) {
+    tft.drawFastVLine(x, 0, tft.height(), color2);
+  }
+}
+
+void testdrawrects(uint16_t color) {
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
+  }
+}
+
+void testfillrects(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST7735_BLACK);
+  for (int16_t x=tft.width()-1; x > 6; x-=6) {
+    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
+  }
+}
+
+void testfillcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
+    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
+    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testtriangles() {
+  tft.fillScreen(ST7735_BLACK);
+  int color = 0xF800;
+  int t;
+  int w = 63;
+  int x = 159;
+  int y = 0;
+  int z = 127;
+  for(t = 0 ; t <= 15; t+=1) {
+    tft.drawTriangle(w, y, y, x, z, x, color);
+    x-=4;
+    y+=4;
+    z-=4;
+    color+=100;
+  }
+}
+
+void testroundrects() {
+  tft.fillScreen(ST7735_BLACK);
+  int color = 100;
+  int i;
+  int t;
+  for(t = 0 ; t <= 4; t+=1) {
+    int x = 0;
+    int y = 0;
+    int w = 127;
+    int h = 159;
+    for(i = 0 ; i <= 24; i+=1) {
+      tft.drawRoundRect(x, y, w, h, 5, color);
+      x+=2;
+      y+=3;
+      w-=4;
+      h-=6;
+      color+=1100;
+    }
+    color+=100;
+  }
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST7735_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST7735_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST7735_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST7735_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST7735_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST7735_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST7735_WHITE);
+  tft.print(" seconds.");
+}
+
+void mediabuttons() {
+  // play
+  tft.fillScreen(ST7735_BLACK);
+  tft.fillRoundRect(25, 10, 78, 60, 8, ST7735_WHITE);
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_RED);
+  delay(500);
+  // pause
+  tft.fillRoundRect(25, 90, 78, 60, 8, ST7735_WHITE);
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_GREEN);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_GREEN);
+  delay(500);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_BLUE);
+  delay(50);
+  // pause color
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_RED);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_RED);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_GREEN);
+}
+
